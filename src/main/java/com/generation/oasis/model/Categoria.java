ee5b91e51a6035f865e89d4b6259f2ebbb5b17dc
Flavia R. Paião <flarpaiao.home@gmail.com>
2024-02-01 15:05:34 -0300

    atualização crud-usuario

--- src/main/java/com/generation/oasis/model/Categoria.java
+++ src/main/java/com/generation/oasis/model/Categoria.java
@@ -1,13 +1,16 @@
 package com.generation.oasis.model;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
-import org.hibernate.annotations.UpdateTimestamp;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -22,8 +25,9 @@ public class Categoria {
 	@NotNull(message = "O atributo título é Obrigatório!")
 	private String tipo;
 	
-	@UpdateTimestamp
-	private LocalDateTime data;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -41,13 +45,12 @@ public class Categoria {
 		this.tipo = tipo;
 	}
 
-	public LocalDateTime getData() {
-		return data;
+	public List<Produto> getProduto() {
+		return produto;
 	}
 
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
-	
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}	
 	
 }
